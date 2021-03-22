5885fab7018740fe9869998129dadeda54ed3215
Tucker Hoog <tuckerhoog@tutanota.com>
2021-03-22 16:24:36 -0400

    added inputs and outputs

--- app.js
+++ app.js
@@ -3,3 +3,13 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+const nounOneInput = document.getElementById('noun-1');
+
+const nounOneOutput = document.getElementById('noun-1-output');
+
+const button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+    nounOneOutput.textContent = nounOneInput.value;
+});
